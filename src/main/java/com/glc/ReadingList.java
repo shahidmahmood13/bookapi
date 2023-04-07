fa3a30890cf677fe49331ada2504b1832ec6e958
shahidmahmood13 <shahid.mehmood@xloopdigital.com>
2023-04-07 10:42:06 +0500

    second testcase passed commit

--- src/main/java/com/glc/ReadingList.java
+++ src/main/java/com/glc/ReadingList.java
@@ -6,12 +6,29 @@ public class ReadingList {
     public String dateRead;
     public int Rating;
 
-public List<String> bookList = new ArrayList<>(); 
+    private class BookInfo{
+        int Rating;
+        String DateRead;
+        Book obj;
+        public BookInfo(int rating, String dateRead, Book obj) {
+            Rating = rating;
+            DateRead = dateRead;
+            this.obj = obj;
+        }
+        
+        
+    }
+public List<BookInfo> bookList = new ArrayList<>(); 
 
 
 public void addBook(Book obj, String DateRead, int Rating){
-
+   
+    
+// bookList.add(obj.title="abc",obj.author="huda",obj.year=2021,obj.length=294,Rating=2, dateRead = "january-12-2021" );/
+BookInfo bookifo= new BookInfo(Rating, DateRead, obj);
+bookList.add(bookifo);
 }
+
 
 
 public int numberRead(){
